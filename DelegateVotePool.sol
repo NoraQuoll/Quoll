1c3e188b439c11ab66df6e59f6b18c5eee85c031
quollfi <tech@quoll.finance>
2022-12-09 17:19:51 +0800

    remove redundant import

--- DelegateVotePool.sol
+++ DelegateVotePool.sol
@@ -5,7 +5,6 @@ pragma experimental ABIEncoderV2;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 import "@shared/lib-contracts/contracts/Dependencies/ManagerUpgradeable.sol";
 import "./Interfaces/IBribeManager.sol";
