98d6810553ddeca79a2ddf21dbc3c86396b51387
Sotatek-DatTran2 <dat.tran2@sotatek.com>
2025-03-17 15:01:46 +0700

    feat: new ICO contracts

---
+++ contracts/ICO/ICOPrivateSale.sol
@@ -0,0 +1,77 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import "@openzeppelin/contracts/math/Math.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+import "../lib/ManagerUpgradeable.sol";
+
+/// @notice In private sale, whitelists will have a precise allocation per user
+contract ICOPrivateSale is ManagerUpgradeable {
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
+
+    IERC20 public usdt; //USDT
+
+    uint256 public tokenPrice; //10000 = 0.01 USDT
+    uint256 public minBuyAmount;
+
+    uint256 public sold;
+    uint256 public endTime; //user can not buy after this time
+
+    address recipient; //receive payment
+
+    mapping(address => uint256) public limitAmounts;
+    mapping(address => uint256) public totalAmounts;
+    event UserBought(address indexed _user, uint256 _amount);
+
+    function initialize(
+        address _usdt,
+        uint256 _tokenPrice,
+        uint256 _minBuyAmount,
+        uint256 _endTime,
+        address _recipient
+    ) public initializer {
+        __Ownable_init();
+        require(_usdt != address(0), "invalid _usdt!");
+        require(_tokenPrice > 0, "invalid _tokenPrice");
+        require(_minBuyAmount > 0, "invalid _minBuyAmount");
+        require(_endTime > block.timestamp, "invalid _endTime");
+        require(_recipient != address(0), "invalid _recipient");
+
+        usdt = IERC20(_usdt);
+        tokenPrice = _tokenPrice;
+        minBuyAmount = _minBuyAmount;
+        endTime = _endTime;
+        recipient = _recipient;
+    }
+
+    function allocate(address[] calldata _users, uint256[] calldata _limitAmounts) external onlyManager {
+        for (uint i = 0; i < _users.length; i++) {
+            require(limitAmounts[_users[i]]==0, " already allocated");
+            limitAmounts[_users[i]] = _limitAmounts[i];
+        }
+    }
+
+
+    //to change the address receive payment
+    function setRecipient(address _recipient) external onlyManager {
+        require(_recipient != address(0), "!invalid _recipient");
+        recipient = _recipient;
+    }
+
+
+    function buy(uint256 _usdAmount) external {
+        uint256 tokenAmount = _usdAmount.mul(10 ** 18).div(tokenPrice);
+
+        require(tokenAmount <= limitAmounts[msg.sender], "exceeds allocated amount");
+
+        require(block.timestamp < endTime, "can not buy this time!");
+        require(_usdAmount >= minBuyAmount, "insufficient purchase amount!");
+        sold = sold.add(tokenAmount);
+        usdt.transferFrom(msg.sender, recipient, _usdAmount);
+        totalAmounts[msg.sender] = totalAmounts[msg.sender].add(tokenAmount);
+        emit UserBought(msg.sender, tokenAmount);
+    }
+}
