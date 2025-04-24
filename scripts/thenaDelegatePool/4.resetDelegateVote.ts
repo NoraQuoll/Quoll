542565b2ec1670539cb00ffa04b82428b8754540
Sotatek-DatTran2 <dat.tran2@sotatek.com>
2025-04-24 11:16:14 +0700

    chore: update affected user

--- scripts/thenaDelegatePool/4.resetDelegateVote.ts
+++ scripts/thenaDelegatePool/4.resetDelegateVote.ts
@@ -20,15 +20,9 @@ const user = web3.eth.accounts.privateKeyToAccount(user_pk!).address;
 const thenaDelegatePool = "0x5d34F95157558af63dfD8091dA329D36Fe5C64b6";
 
 const users = [
-    '0x4015786119590E79Bc0C28Be82f64c3e9A7c01d0',
-    '0x7F5362951c7Ba111c5e196b7DC6c8F61b876e345',
-    '0xfA2a6Feb4D3e11c650ba067ea53e24a42EC935Eb',
-    '0x7307E16a2C4d197AFd2498E42F5327Ee0b72e78F',
-    '0x429AD64Fec22Bc0616D56ED2A48D32dBc9C8D0D6',
-    '0xd6B8BbcD887f2FF49465e4624E11D2bBf42E1c41',
-    '0xC1F466b79bd98Fb0b14a9504eAae0b41acb2AB82',
-    '0x78C12f471dd8227E96fe0989cAA4cB7e17b29fE0'
-];
+    '0x51bb4bd9be3d250F25D8717678BD4dB681270450',
+    '0xdE8E0c378b50118b30512386dEE6b0475D96173A'
+  ];
 async function main() {
     const ThenaDelegatePool = JSON.parse(
         fs.readFileSync(
