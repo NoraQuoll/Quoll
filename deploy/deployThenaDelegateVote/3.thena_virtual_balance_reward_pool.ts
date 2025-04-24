6f54d8cfbd45e9dc9447abb5d59b12194436e5b0
Sotatek-ThaoNguyen3 <thao.nguyen3@sotatek.com>
2025-04-24 19:45:54 +0700

    upgrade contract vitual balance

--- deploy/deployThenaDelegateVote/3.thena_virtual_balance_reward_pool.ts
+++ deploy/deployThenaDelegateVote/3.thena_virtual_balance_reward_pool.ts
@@ -24,10 +24,10 @@ const deploy: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     proxy: {
       proxyContract: "OptimizedTransparentProxy",
       owner: deployer,
-    execute: {
-        methodName: "initialize",
-        args: [operator],
-      },
+    // execute: {
+    //     methodName: "initialize",
+    //     args: [operator],
+    //   },
     },
   });
 
